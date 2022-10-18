dc6a1d62e0c2860ce0f1fa8f6f6ed34051b7ad5f
ThyBui07 <buithinguyenthy@gmail.com>
2022-10-18 21:51:48 +0300

    set header

--- server/getHandler.go
+++ server/getHandler.go
@@ -3,6 +3,7 @@ package server
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 //create a variable from server package
@@ -22,8 +23,12 @@ func GetRequest(w http.ResponseWriter, r *http.Request) {
 
 func GetDownloadFile(w http.ResponseWriter, r *http.Request) {
 	b := []byte(FinalText)
+	leng := strconv.Itoa(len(b))
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", "attachement")
+	w.Header().Set("Content-Length", leng)
+
 	w.Write(b)
 	return
 }
